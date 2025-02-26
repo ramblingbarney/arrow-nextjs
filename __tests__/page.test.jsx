c1902c120b163c5ff8d954f5741859e44aed8d9f
Conor <c9dw5er@protonmail.com>
2025-02-26 19:09:43 +0000

    Turn Off Features Menu

--- __tests__/page.test.jsx
+++ __tests__/page.test.jsx
@@ -11,9 +11,52 @@ describe("Page", () => {
     expect(image.classList.contains("logo-clockwise")).toBe(true);
   });
 
+  it("renders a hamburger", () => {
+    render(<Page />);
+    const hamburger = screen.getByTestId("hamburger-icon");
+
+    expect(hamburger).toBeInTheDocument();
+    expect(hamburger.classList.contains("HAMBURGER-ICON")).toBe(true);
+  });
+
+  it("click on hamburger to get menu", () => {
+    render(<Page />);
+    const hamburger = screen.getByTestId("hamburger-icon");
+    fireEvent.click(hamburger);
+    const activateDirection = screen.getByText("Turn Direction Off");
+
+    expect(activateDirection).toBeInTheDocument();
+  });
+
+  it("click on menu item changes text", () => {
+    render(<Page />);
+    const hamburger = screen.getByTestId("hamburger-icon");
+    fireEvent.click(hamburger);
+
+    const activateDirection = screen.getByText("Turn Direction Off");
+    fireEvent.click(activateDirection);
+    const activateDirectionOn = screen.getByText("Turn Direction On");
+
+    const activateMouseMove = screen.getByText("Turn MouseMove Off");
+    fireEvent.click(activateMouseMove);
+    const activateMouseMoveOn = screen.getByText("Turn MouseMove On");
+
+    const activateMouseTime = screen.getByText("Turn MouseTime Off");
+    fireEvent.click(activateMouseTime);
+    const activateMouseTimeOn = screen.getByText("Turn MouseTime On");
+
+    expect(activateDirectionOn).toBeInTheDocument();
+    expect(activateMouseMoveOn).toBeInTheDocument();
+    expect(activateMouseTimeOn).toBeInTheDocument();
+  });
+
   it("click on image changes rotation direction", () => {
     render(<Page />);
     const image = screen.getByRole("img");
+    const hamburger = screen.getByTestId("hamburger-icon");
+    fireEvent.click(hamburger);
+    const activateDirection = screen.getByText("Turn Direction Off");
+    fireEvent.click(activateDirection);
     fireEvent.click(image);
 
     expect(image.classList.contains("logo-anticlockwise")).toBe(true);
@@ -22,28 +65,34 @@ describe("Page", () => {
   it("renders a image with expected dimensions", () => {
     render(<Page />);
     const image = screen.getByRole("img");
-
-    expect(image.width).toBe(10);
-    expect(image.height).toBe(10);
+    expect(image.width).toBe(50);
+    expect(image.height).toBe(50);
   });
 
   it("renders a image with expected dimensions after mouse event", () => {
     render(<Page />);
     const image = screen.getByRole("img");
+    const hamburger = screen.getByTestId("hamburger-icon");
+    fireEvent.click(hamburger);
+    const activateMouseMove = screen.getByText("Turn MouseMove Off");
+    fireEvent.click(activateMouseMove);
     fireEvent.mouseMove(image, {
-      clientX: 500,
-      clientY: 500,
+      clientX: 600,
+      clientY: 600,
     });
 
-    expect(image.width).toBe(50);
-    expect(image.height).toBe(50);
+    expect(image.width).toBe(60);
+    expect(image.height).toBe(60);
   });
 
-  it("renders time exlapsed since last mouse move event", async () => {
+  it("renders time elapsed since last mouse move event", async () => {
     render(<Page />);
     const image = screen.getByRole("img");
+    const hamburger = screen.getByTestId("hamburger-icon");
+    fireEvent.click(hamburger);
+    const activateMouseTime = screen.getByText("Turn MouseTime Off");
+    fireEvent.click(activateMouseTime);
 
-    const currentTimeStamp = new Date().getTime();
     fireEvent.mouseMove(image, {
       clientX: 500,
       clientY: 500,
