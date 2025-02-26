c3a663a12e3919a8e6af38cc345e67425d037ee7
Conor <c9dw5er@protonmail.com>
2025-02-26 19:09:43 +0000

    Turn Off Features Menu

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,26 @@ export default function Home() {
   const [imageDimensions, setImageDimensions] = useState<{
     x: number;
     y: number;
-  }>({ x: 10, y: 10 });
+  }>({ x: 50, y: 50 });
 
   const [lastTimeStamp, setLastTimeStamp] = useState<number>(0);
   const [displaySeconds, setDisplaySeconds] = useState<number>(0);
 
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isDirectionActive, setIsDirectionActive] = useState(false);
+  const [isMouseMoveActive, setIsMouseMoveActive] = useState(false);
+  const [isMouseTimeActive, setIsMouseTimeActive] = useState(false);
+
   const handleLogoClick = () => {
     setDirection(!direction);
+  };
+
+  const handleDirectionActive = () => {
+    setIsDirectionActive(!isDirectionActive);
+  };
+
+  const handleMouseTimeActive = () => {
+    setIsMouseTimeActive(!isMouseTimeActive);
   };
 
   const calculateDisplaySeconds = () => {
@@ -38,6 +51,10 @@ export default function Home() {
     }
   };
 
+  const handleMouseMoveActive = () => {
+    setIsMouseMoveActive(!isMouseMoveActive);
+  };
+
   useEffect(() => {
     target.current = document.querySelector(".container");
     target.current?.addEventListener("mousemove", handleMouseMove);
@@ -45,21 +62,71 @@ export default function Home() {
 
   return (
     <div className="container">
-      <Image
-        src={reactLogo}
-        width={imageDimensions.x}
-        height={imageDimensions.y}
-        className={
-          direction
-            ? "logo react logo-anticlockwise"
-            : "logo react logo-clockwise"
-        }
-        alt="React logo"
-        onClick={handleLogoClick}
-      />
-      <h1 className="title">
-        Time Mouse Stationary <span>{displaySeconds}</span>
-      </h1>
+      <div className="sidebar" onClick={() => setIsNavOpen((prev) => !prev)}>
+        <div
+          className={isNavOpen ? "hidden" : "HAMBURGER-ICON space-y-2"}
+          data-testid="hamburger-icon"
+        >
+          <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+        </div>
+        <div
+          className={isNavOpen ? "" : "hidden"}
+          onClick={() => setIsNavOpen((prev) => !prev)}
+        >
+          <ul className="flex flex-col items-center justify-between min-h-[250px]">
+            <li className="border-b border-gray-600 my-8 uppercase">
+              <button onClick={handleDirectionActive} className="nav-text">
+                Turn Direction {isDirectionActive ? "On" : "Off"}
+              </button>
+            </li>
+            <li className="border-b border-gray-600 my-8 uppercase">
+              <button onClick={handleMouseMoveActive} className="nav-text">
+                Turn MouseMove {isMouseMoveActive ? "On" : "Off"}
+              </button>
+            </li>
+            <li className="border-b border-gray-600 my-8 uppercase">
+              <button onClick={handleMouseTimeActive} className="nav-text">
+                Turn MouseTime {isMouseTimeActive ? "On" : "Off"}
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="logo">
+        {isMouseMoveActive ? (
+          <Image
+            src={reactLogo}
+            width={imageDimensions.x}
+            height={imageDimensions.y}
+            className={
+              direction && isDirectionActive
+                ? "logo react logo-anticlockwise"
+                : "logo react logo-clockwise"
+            }
+            alt="React logo"
+            onClick={handleLogoClick}
+          />
+        ) : (
+          <Image
+            src={reactLogo}
+            width={50}
+            height={50}
+            className={
+              direction && isDirectionActive
+                ? "logo react logo-anticlockwise"
+                : "logo react logo-clockwise"
+            }
+            alt="React logo"
+            onClick={handleLogoClick}
+          />
+        )}
+        <h1 className="title">
+          Time Mouse Stationary{" "}
+          <span>{isMouseTimeActive ? displaySeconds : 0}</span>
+        </h1>
+      </div>
     </div>
   );
 }
